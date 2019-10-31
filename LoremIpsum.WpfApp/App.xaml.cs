5006400f2d71de6b74cd81c031c91588f4aefe20
Nelson <villacruznelson@gmail.com>
2019-10-31 08:59:57 +0800

    Code cleanup

---
+++ LoremIpsum.WpfApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace LoremIpsum.WpfApp.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
